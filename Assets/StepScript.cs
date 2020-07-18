a1562620719a0326089b7ad4c72d99b1d413846d
Erik <uss593@my.utsa.edu>
2020-07-18 15:07:04 -0500

    connected all leg joints to control script

--- Assets/StepScript.cs
+++ Assets/StepScript.cs
@@ -9,27 +9,66 @@ public class StepScript : MonoBehaviour
     public GameObject FLThigh;
     public GameObject FRShin;
     public GameObject FLShin;
+    public GameObject RRThigh;
+    public GameObject RLThigh;
+    public GameObject RRShin;
+    public GameObject RLShin;
 
-    public float x_torque = 1000f;
+    public float thighPosition = 0.0f;
+    public float shinPosition = 0.0f;
 
-    Rigidbody fr_rb;
-    Rigidbody fl_rb;
+    Rigidbody frt_rb;
+    Rigidbody flt_rb;
+    Rigidbody frs_rb;
+    Rigidbody fls_rb;
 
-    HingeJoint r_joint;
+    HingeJoint frtJoint;
+    HingeJoint fltJoint;
+    HingeJoint rrtJoint;
+    HingeJoint rltJoint;
+    HingeJoint frsJoint;
+    HingeJoint flsJoint;
+    HingeJoint rrsJoint;
+    HingeJoint rlsJoint;
 
+    public void SetJointTargetRotation(HingeJoint joint, float x) {
+        x = (x + 1f) * 0.5f;
+
+        var xRot = Mathf.Lerp(joint.limits.min, joint.limits.max, x);
+
+        JointSpring hingeSpring = joint.spring;
+        joint.useSpring = true;
+        hingeSpring.spring = 1000f;
+        hingeSpring.damper = 0.5f;
+        hingeSpring.targetPosition = xRot;
+        joint.spring = hingeSpring;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        r_joint = FRThigh.GetComponent<HingeJoint>();
-        fr_rb = FRThigh.GetComponent<Rigidbody>();
-        fl_rb = FLThigh.GetComponent<Rigidbody>();
+        fltJoint = FLThigh.GetComponent<HingeJoint>();
+        frtJoint = FRThigh.GetComponent<HingeJoint>();
+        rrtJoint = RRThigh.GetComponent<HingeJoint>();
+        rltJoint = RLThigh.GetComponent<HingeJoint>();
 
+        frsJoint = FRShin.GetComponent<HingeJoint>();
+        flsJoint = FLShin.GetComponent<HingeJoint>();
+        rrsJoint = RRShin.GetComponent<HingeJoint>();
+        rlsJoint = RLShin.GetComponent<HingeJoint>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // r_joint.targetPosition = new Vector3(-5, 0, 0);
+        SetJointTargetRotation(frtJoint, thighPosition);
+        SetJointTargetRotation(fltJoint, thighPosition);
+        SetJointTargetRotation(rrtJoint, thighPosition);
+        SetJointTargetRotation(rltJoint, thighPosition);
+
+        SetJointTargetRotation(frsJoint, shinPosition);
+        SetJointTargetRotation(flsJoint, shinPosition);
+        SetJointTargetRotation(rrsJoint, shinPosition);
+        SetJointTargetRotation(rlsJoint, shinPosition);
     }
 }
